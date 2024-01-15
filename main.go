b3a1b0fc22094e27b35da82b275f5997cfe7c7a8
khanh <khanhyou2018@gmail.com>
2024-01-15 14:48:56 +0700

    add commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("hello world")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the enviroment")
+	}
+
+	fmt.Println("PORT", portString)
+}
