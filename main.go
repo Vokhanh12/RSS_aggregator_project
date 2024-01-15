6271311a996a69ee41882a58ac71effbf3799c1f
khanh <khanhyou2018@gmail.com>
2024-01-15 16:17:19 +0700

    add commit

--- main.go
+++ main.go
@@ -3,17 +3,52 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	fmt.Println("hello world")
 
+	godotenv.Load()
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the enviroment")
 	}
 
-	fmt.Println("PORT", portString)
+	route := chi.NewRouter()
+
+	route.Use(cors.Handler(cors.Options{
+
+		AllowedOrigins:   []string{"https//*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	route.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: route,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
